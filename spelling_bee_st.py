2e2f31a661b4083f928fceba187cf9668f119305
jte2004 <jte2004@nyu.edu>
2025-01-10 01:44:59 -0500

    slim down code more

--- spelling_bee_st.py
+++ spelling_bee_st.py
@@ -17,11 +17,6 @@ EXCLUDE_WORD_LIST = [
 ]
 
 
-def letter_check(word: str, letter_list: list) -> bool:
-    """Checks if every character in a word is in our letter list."""
-    check_list = [letter in letter_list for letter in word]
-    return all(check_list)
-
 def display_word_column(
     column: st.delta_generator.DeltaGenerator,
     word_list: list,
@@ -33,7 +28,7 @@ def display_word_column(
         for word in word_list[start_index: end_index]:
             st.write(word)
     
-    
+
 #1. READ IN WORDS WITH INITIAL CLEANING
 df_all = pd.read_csv('data/words.csv')
 df_all['word_length'] = df_all.word.str.len()
@@ -42,54 +37,67 @@ df_all['word_length'] = df_all.word.str.len()
 df_all = df_all[(df_all.word_length >= 4) & (~df_all.word.isin(EXCLUDE_WORD_LIST))].copy()
 
 # add words from the custom list
-df_all = pd.concat((
-    df_all, pd.DataFrame(ADDED_WORD_LIST, columns=['word'])
-))
+df_added_words = pd.DataFrame(ADDED_WORD_LIST, columns=['word'])
+df_all = pd.concat((df_all, df_added_words))
 
 #2. STREAMLIT HEADERS
 st.title("NYT Spelling Bee App")
-st.write("The rules of Spelling Bee are simple: you are given 7 letters to make words. One of these letters must be included in every word and all words must only consist of this letter plus some of the other 6. Words must be four letters or longer. Each day, there are 1+ pangram(s), which include all 7 letters.") 
+st.write(
+    "The rules of Spelling Bee are simple: you are given 7 letters to make words. One of these "
+    "letters must be included in every word and all words must only consist of this letter plus "
+    "some of the other 6. Words must be four letters or longer. Each day, there are 1+ pangram(s), "
+    "which include all 7 letters."
+)
 
-st.write("It is worth noting that this is NOT the same source of words the New York Times uses, so the lists this script may output certain words that are not accepted by the game, and could potentially miss some used by the NY Times. I am unable to find the list that they use. If you see a word that the game won't accept or a word missing from this list, please let me know!")
+st.write(
+    "It is worth noting that this is NOT the same source of words the New York Times uses, "
+    "so the lists this script may output certain words that are not accepted by the game, "
+    "and could potentially miss some used by the NY Times. I am unable to find the list that "
+    "they use. If you see a word that the game won't accept or a word missing from this list, "
+    "please let me know!"
+)
     
-st.write("To play the game, go to this link: https://www.nytimes.com/puzzles/spelling-bee. Obviously, I would consider using this app as a form of cheating (but I won't tell.)")
+st.write(
+    "To play the game, go to this link: https://www.nytimes.com/puzzles/spelling-bee. "
+    "Obviously, I would consider using this app as a form of cheating (but I won't tell.)"
+)
 
 #3. INPUT LETTERS FOR DAY'S SPELLING BEE 
 st.header('Letter Selection')
 
 alphabet_list = list(string.ascii_lowercase)
-middle_letter = st.selectbox("Choose Middle Letter", alphabet_list) #user inputs middle letter
 
-# start the list of letters with the middle letter
+# user inputs middle letter
+middle_letter = st.selectbox("Choose Middle Letter", alphabet_list)
 all_letters = [middle_letter]
 
-#add each letter to our list of all letters
-others = st.multiselect("Choose All Other Letters", alphabet_list) #user inputs other letters
-all_letters += others
+# user inputs other letters
+other_letters = st.multiselect("Choose All Other Letters", alphabet_list)
+all_letters += other_letters
     
-
-if len(others) != 6:
-    st.write('Please Choose 6 Additional Letters', color='red')   
+if len(other_letters) > 6:
+    st.write('WARNING: Too many letters', color='red')
+elif len(other_letters) < 6:
+    st.write('Please Choose 6 Additional Letters', color='red')
 else:    
     # 4. NARROW DOWN WORD LIST
     df_middle_letter = df_all[df_all.word.str.contains(middle_letter)].copy()
 
-    #make a column for words containing every letter
-    for i in range(6):
-        df_middle_letter[i] = df_middle_letter.word.str.contains(others[i])    
-    df_middle_letter['all'] = (
-        df_middle_letter[0] & df_middle_letter[1] &
-        df_middle_letter[2] & df_middle_letter[3] &
-        df_middle_letter[4] & df_middle_letter[5]
+    # check for any words that contain every letter
+    df_middle_letter['pangram'] = df_middle_letter['word'].apply(
+        lambda x: all([letter in x for letter in other_letters])
     )
 
-    #apply this function to our list and then shrink dataframe to only have allowed words
-    df_middle_letter['check'] = df_middle_letter['word'].apply(lambda x: letter_check(word=x, letter_list=all_letters))
-    df_final = df_middle_letter[df_middle_letter['check']].copy()
+    # check for words that only contains letters in our list
+    df_middle_letter['word_in_list'] = df_middle_letter['word'].apply(
+        lambda x: all([letter in all_letters for letter in x])
+    )
+
+    df_final = df_middle_letter[df_middle_letter['word_in_list']].copy()
 
     # 5. RESULTS
     st.header("Today's Pangram(s):")
-    pangrams = df_final[df_final['all']]['word'].to_list()
+    pangrams = df_final[df_final['pangram']]['word'].to_list()
     for pangram in pangrams:
         st.subheader(pangram)
     
