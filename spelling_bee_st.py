97d8035135a15fc5c026b3068789cbf55dcd3e86
jte2004 <jte2004@nyu.edu>
2025-01-10 01:31:16 -0500

    large code cleanup

--- spelling_bee_st.py
+++ spelling_bee_st.py
@@ -4,107 +4,103 @@ import streamlit as st
 import string
 
 
-#create a function that takes in a string and checks if every character in that string is in our letter list
-def letter_check(word):
-    check_list = [l in all_letters for l in word] #creates a list of string length with T/F indicator if letter is in list
-    return all(check_list) #returns true only if all true
-    
-    
+ADDED_WORD_LIST = [
+    'naan', 'cocci', 'cutout', 'oniony', 'toon', 'gigging', 'agaze', 'algal', 'deglaze', 'deglazed',
+    'eagled', 'geez', 'gelee', 'gigged', 'glia', 'zagged', 'zigged'
+]
 
+EXCLUDE_WORD_LIST = [
+    'continuo', 'cony', 'coon', 'iconicity', 'initio', 'nicotinic', 'nuncio', 'toto', 'yoyo', 'eigne',
+    'geeing', 'ginnie', 'whig', 'algid', 'diallage', 'gael', 'gail', 'gaza', 'geed', 'gelid', 'lege',
+    'allah', 'althea', 'athena', 'ethel', 'halloo', 'hanna', 'hannah', 'helen', 'helena', 'leah',
+    'loth', 'nathan', 'noah', 'othello'
+]
+
+
+def letter_check(word: str, letter_list: list) -> bool:
+    """Checks if every character in a word is in our letter list."""
+    check_list = [letter in letter_list for letter in word]
+    return all(check_list)
+
+def display_word_column(
+    column: st.delta_generator.DeltaGenerator,
+    word_list: list,
+    start_index: int,
+    end_index: int
+) -> None:
+    """Display part of a list as a column of words."""
+    with column:
+        for word in word_list[start_index: end_index]:
+            st.write(word)
+    
+    
 #1. READ IN WORDS WITH INITIAL CLEANING
-st.title('NYT Spelling Bee App')
+df_all = pd.read_csv('data/words.csv')
+df_all['word_length'] = df_all.word.str.len()
+
+# filter out words under 4 letters or in the exclude list
+df_all = df_all[(df_all.word_length >= 4) & (~df_all.word.isin(EXCLUDE_WORD_LIST))].copy()
+
+# add words from the custom list
+df_all = pd.concat((
+    df_all, pd.DataFrame(ADDED_WORD_LIST, columns=['word'])
+))
+
+#2. STREAMLIT HEADERS
+st.title("NYT Spelling Bee App")
 st.write("The rules of Spelling Bee are simple: you are given 7 letters to make words. One of these letters must be included in every word and all words must only consist of this letter plus some of the other 6. Words must be four letters or longer. Each day, there are 1+ pangram(s), which include all 7 letters.") 
 
 st.write("It is worth noting that this is NOT the same source of words the New York Times uses, so the lists this script may output certain words that are not accepted by the game, and could potentially miss some used by the NY Times. I am unable to find the list that they use. If you see a word that the game won't accept or a word missing from this list, please let me know!")
     
 st.write("To play the game, go to this link: https://www.nytimes.com/puzzles/spelling-bee. Obviously, I would consider using this app as a form of cheating (but I won't tell.)")
 
-
-#read in words 
-df = pd.read_csv('data/words.csv')
-
-df['word_length'] = df.word.str.len() #get column of word length
-df = df.loc[df.word_length >= 4] #filter out words shorter than 4 letters
-
-# save list of words not in new york times list -- can be updated
-not_in_list = ['continuo','cony','coon','iconicity','initio','nicotinic','nuncio','toto','yoyo','eigne','geeing',
-              'ginnie','whig','algid','diallage','gael','gail','gaza','geed','gelid','lege','allah','althea','athena',
-              'ethel','halloo','hanna','hannah','helen','helena','leah','loth','nathan','noah','othello']
-df = df.loc[~df.word.isin(not_in_list)].copy() #filter these out
-
-# save list of words missing from our dataset -- can be updated
-missing = ['naan','cocci','cutout','oniony','toon','gigging','agaze','algal','deglaze','deglazed','eagled','geez','gelee',
-          'gigged','glia','zagged','zigged']
-df = pd.concat((df, pd.DataFrame(missing,columns=['word'])))
-
-
-#2. INPUT LETTERS FOR DAY'S SPELLING BEE 
+#3. INPUT LETTERS FOR DAY'S SPELLING BEE 
 st.header('Letter Selection')
 
-alphabet_string = string.ascii_lowercase
-alphabet_list = list(alphabet_string)
+alphabet_list = list(string.ascii_lowercase)
+middle_letter = st.selectbox("Choose Middle Letter", alphabet_list) #user inputs middle letter
 
-all_letters = [] #initiate an empty list to hold letters
-middle_letter = st.selectbox("Choose Middle Letter",alphabet_list) #user inputs middle letter
-all_letters.append(middle_letter) #append middle letter to the list of all letters
-
-
-others = st.multiselect("Choose All Other Letters",alphabet_list) #user inputs other letters
+# start the list of letters with the middle letter
+all_letters = [middle_letter]
 
 #add each letter to our list of all letters
-for o in others:
-    all_letters.append(o)
+others = st.multiselect("Choose All Other Letters", alphabet_list) #user inputs other letters
+all_letters += others
     
 
 if len(others) != 6:
-    st.write('Please Choose 6 Additional Letters', color='red')
-    
+    st.write('Please Choose 6 Additional Letters', color='red')   
 else:    
-
-    # 3. NARROW DOWN WORD LIST
-    df = df.loc[df.word.str.contains(middle_letter)] #all words need middle letter
+    # 4. NARROW DOWN WORD LIST
+    df_middle_letter = df_all[df_all.word.str.contains(middle_letter)].copy()
 
     #make a column for words containing every letter
     for i in range(6):
-        df[i] = df.word.str.contains(others[i])    
-    df['all'] = df[0] & df[1] & df[2] & df[3] & df[4] & df[5]
-
+        df_middle_letter[i] = df_middle_letter.word.str.contains(others[i])    
+    df_middle_letter['all'] = (
+        df_middle_letter[0] & df_middle_letter[1] &
+        df_middle_letter[2] & df_middle_letter[3] &
+        df_middle_letter[4] & df_middle_letter[5]
+    )
 
     #apply this function to our list and then shrink dataframe to only have allowed words
-    df['check'] = df.apply(lambda x: letter_check(x['word']), axis=1)
-    df = df.loc[df['check']].copy()
+    df_middle_letter['check'] = df_middle_letter['word'].apply(lambda x: letter_check(word=x, letter_list=all_letters))
+    df_final = df_middle_letter[df_middle_letter['check']].copy()
 
-
-
-    # 4. RESULTS
+    # 5. RESULTS
     st.header("Today's Pangram(s):")
-    for w in df.loc[df['all']]['word']:
-        st.subheader(w)
-
-    n_words = df.shape[0]  
+    pangrams = df_final[df_final['all']]['word'].to_list()
+    for pangram in pangrams:
+        st.subheader(pangram)
+    
+    n_words = df_final.shape[0]  
     n_words_4 = np.ceil(n_words/4).astype(int)
 
-    st.header("Full List of Words (total={}):".format(n_words))
-    words = list(df['word'])
-
+    st.header(f"Full List of Words (total={n_words}):")
+    words = df_final['word'].to_list()
 
     col1, col2, col3, col4 = st.columns(4)
-
-    with col1:
-        for i in words[0:n_words_4]:
-            st.write(i)
-
-    with col2:
-        for i in words[n_words_4:2*n_words_4]:
-            st.write(i)
-
-    with col3:
-        for i in words[2*n_words_4:3*n_words_4]:
-            st.write(i)
-
-    with col4:
-        for i in words[3*n_words_4:]:
-            st.write(i)
-
-    
-    
+    display_word_column(col1, words, 0, n_words_4)
+    display_word_column(col2, words, n_words_4, 2 * n_words_4)
+    display_word_column(col3, words, 2 * n_words_4, 3 * n_words_4)
+    display_word_column(col4, words, 3 * n_words_4, n_words)
